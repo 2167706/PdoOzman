32c0b8393633e3078b38b1a62c2ca628bcda70a7
2167706 <133228756+2167706@users.noreply.github.com>
2023-11-20 16:59:24 +0100

    Add files via upload

---
+++ op1.php
@@ -0,0 +1,16 @@
+<?php
+class Database {
+    public $pdo;
+ 
+    public function __construct($host = "localhost:3306", $user = "root", $pass = "", $db = "test") {
+ 
+        try {
+            $this->pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "connected to $db";
+        } catch (Exception $e){
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+}
+?>
