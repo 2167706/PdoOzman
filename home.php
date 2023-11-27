ae227b167463df36b30d4986692a1f998b81ba2e
2167706 <133228756+2167706@users.noreply.github.com>
2023-11-27 21:16:24 +0100

    Add files via upload

--- home.php
+++ home.php
@@ -1,10 +1,12 @@
 <?php
-include('op1.php');
+include 'db.php';
+$pdo = new Database();
 
-$db = new Database ();
-
-
-
-
-
+try {
+    $pdo->insertUser("banaan", "banaan");
+    echo "succes";
+} 
+catch (PDOException $e) {
+    echo "error instert .".$e->getMessage();
+}
 ?>
